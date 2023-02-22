f8c40c6ee9d2390452611241f8c84a62217c4dc7
m2lovric <m2lovric@gmail.com>
2023-02-22 22:40:55 +0100

    Add mobile styling

--- src/App.tsx
+++ src/App.tsx
@@ -26,9 +26,9 @@ function App() {
 
   return (
     <main className='bg-neutral-900 h-screen w-full flex justify-around text-white'>
-      <section className='w-1/2 pt-28 font-Inter'>
+      <section className='w-1/2 pt-28 font-Inter max-sm:w-full max-sm:pt-20 max-sm:px-6'>
         <h1 className='text-4xl font-bold'>My technologies</h1>
-        <section className='w-full flex items-center mt-14'>
+        <section className='w-full flex items-center mt-14 max-sm:flex-col max-sm:items-start'>
           <form onSubmit={(e) => AddItem(e)} className='flex'>
             <input
               type='text'
@@ -46,17 +46,17 @@ function App() {
           </form>
           <p
             onClick={RemoveAll}
-            className='ml-auto underline text-rose-300 font-light cursor-pointer'
+            className='ml-auto underline text-rose-300 font-light cursor-pointer max-sm:ml-0 max-sm:mt-5'
           >
             Remove all items
           </p>
         </section>
-        <section className='flex flex-wrap justify-between mt-14'>
+        <section className='flex flex-wrap justify-between mt-14 max-sm:flex-col max-sm:justify-start'>
           {technologies.length > 0 &&
             technologies.map((tech) => (
               <article
                 key={tech.id}
-                className='bg-neutral-800 w-[48%] py-3 text-center mb-7 rounded-lg shadow-lg'
+                className='bg-neutral-800 w-[48%] py-3 text-center mb-7 rounded-lg shadow-lg max-sm:w-full'
               >
                 <p className='text-2xl font-bold'>{tech.data}</p>
               </article>
